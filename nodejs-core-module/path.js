4c81b7cc7a29d951f91ca462c43f52f96b85325a
vishalmishradev <vishalmishra@medianv.com>
2026-02-20 16:21:36 +0530

    modified fs file

--- nodejs-core-module/path.js
+++ nodejs-core-module/path.js
@@ -1,5 +1,5 @@
 // The Path module in Node.js is used to work with file and folder paths safely.
-
+const fs = require('fs');
 const path = require('path');
 const filepath = path.join("folder", 'file.txt');
 console.log(filepath);
@@ -10,3 +10,9 @@ const result = path.parse("C:/folder/file.txt");
 console.log(result);
 
 console.log(path.resolve("file.txt"));
+
+console.log(__dirname);
+
+const pathfile =path.join(__dirname, "file.txt");
+const data = fs.readFileSync(pathfile,'utf8');
+console.log(data);
